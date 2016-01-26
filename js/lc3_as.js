ebe072e23fb6c2cafb4db216bc8f2bd80467992d
William Chargin <wchargin@gmail.com>
2016-01-25 19:00:19 -0500

    Add a polyfill for `includes`
    Test Plan:
    I don't have access to an old enough browser to test this.
    But it doesn't break on Chrome 48!

--- js/lc3_as.js
+++ js/lc3_as.js
@@ -30,7 +30,23 @@ var assemble = (function() {
         return false;
       };
     }
-
+    if (!String.prototype.includes) {
+        String.prototype.includes = function() {
+            'use strict';
+            if (typeof arguments[1] === "number") {
+                if (this.length < arguments[0].length + arguments[1].length) {
+                    return false;
+                } else {
+                    if (this.substr(arguments[1], arguments[0].length) === arguments[0])
+                        return true;
+                    else
+                        return false;
+                }
+            } else {
+                return String.prototype.indexOf.apply(this, arguments) !== -1;
+            }
+        };
+    }
     /*
      * Decorate the given function such that it will always return
      * an object with a boolean "success" key
